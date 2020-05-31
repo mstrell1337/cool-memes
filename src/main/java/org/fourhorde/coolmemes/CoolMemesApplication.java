bf69f18cd02c5c9a9d96c9ac8adb6911f71245d1
mstrell <mstrell1337@gmail.com>
2020-05-31 21:33:13 +0300

    Create war file for Tomcat server

--- src/main/java/org/fourhorde/coolmemes/CoolMemesApplication.java
+++ src/main/java/org/fourhorde/coolmemes/CoolMemesApplication.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CoolMemesApplication {
+public class CoolMemesApplication extends SpringBootServletInitializer {
 	private static final Logger log = LoggerFactory.getLogger(CoolMemesApplication.class);
 
 	public static void main(String[] args) {
